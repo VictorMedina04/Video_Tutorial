ebf03aac105581511bcea7754299323e2712c4d9
Victor <victorml2204@gmail.com>
2023-12-21 18:44:46 +0100

    sv

---
+++ Video_Tutorial/src/video/Principal.java
@@ -0,0 +1,117 @@
+package video;
+
+import utilidades.Leer;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		int opcion, tamanyo = 20;
+		String nombre;
+		int seccion, lecturaVenta, numProductos = 0, numTrabajadores = 0, id;
+		double precioFabrica, ganancias, nuevoPrecio, descuento;
+		boolean enVenta = false;
+		Producto producto;
+		Producto[] listado;
+		Tienda tienda;
+		listado = new Producto[tamanyo];
+		tienda = new Tienda(listado, numProductos, numTrabajadores);
+		
+
+		do {
+			System.out.println("");
+			System.out.println("Gestión de productos");
+			System.out.println("1-Agregar producto");
+			System.out.println("2-Mostrar lista");
+			System.out.println("3-Buscar por sección");
+			System.out.println("4-Mostrar activos");
+			System.out.println("5-Buscar por id un producto");
+			System.out.println("6-Calcular PVP");
+			System.out.println("7-Cambiar el precio por seccion");
+			System.out.println("8-Hacer descuento");
+			System.out.println("9-Borrar sección");
+			System.out.println("0-Salir");
+			opcion = Leer.datoInt();
+
+			switch (opcion) {
+			case 1:
+				System.out.println("Introduzca el nombre");
+				nombre = Leer.dato();
+				System.out.println("Introduzca el id");
+				id = Leer.datoInt();
+				System.out.println("Introduzca precio de fábrica");
+				precioFabrica = Leer.datoDouble();
+				System.out.println("Introduzca el número de la sección");
+				seccion = Leer.datoInt();
+				System.out.println("Introduzca si esta en venta o no \n 1-Si 2-No");
+				lecturaVenta = Leer.datoInt();
+				if (lecturaVenta == 1) {
+					enVenta = true;
+				} else {
+					enVenta = false;
+				}
+				producto = new Producto(nombre, precioFabrica, seccion, enVenta, id);
+
+				tienda.add(producto);
+
+				break;
+			case 2:
+				tienda.mostrarArray(listado);
+				break;
+			case 3:
+				System.out.println("Diga sección");
+				seccion = Leer.datoInt();
+				tienda.mostrarArray(tienda.buscarBySeccion(seccion));
+				break;
+			case 4:
+				tienda.mostrarActivos();
+				break;
+			case 5:
+				System.out.println("Introduzca el id");
+				id = Leer.datoInt();
+				producto = tienda.buscarIDV2(id);
+
+				if (producto == null) {
+					System.out.println("Producto no encontrado");
+				} else {
+					System.out.println(producto);
+				}
+				break;
+			case 6:
+				System.out.println("Introduzca el id");
+				id = Leer.datoInt();
+				System.out.println("Diaga cuanto porcentaje quierer ganar");
+				ganancias = Leer.datoDouble();
+				System.out.printf("El precio de venta es: %.2f €\n", tienda.calcularPrecioFinal(ganancias, id));
+				break;
+			case 7:
+				System.out.println("Introduzca el nuevo precio que quiere");
+				nuevoPrecio = Leer.datoDouble();
+				System.out.println("Indique la seccion");
+				seccion = Leer.datoInt();
+				tienda.modificarPrecioPorSeccion(seccion, nuevoPrecio);
+				break;
+			case 8:
+				System.out.println("Introduzca el descuento");
+				descuento = Leer.datoDouble();
+				System.out.println("Diga las ganancias");
+				ganancias = Leer.datoDouble();
+				System.out.println("Introduzca el id");
+				id = Leer.datoInt();
+				System.out.printf("El producto con descuento es: %.2f",
+						tienda.hacerDescuento(descuento, ganancias, id));
+				break;
+			case 9:
+				System.out.println("Indique la seccion");
+				seccion = Leer.datoInt();
+				tienda.eliminarUnaSeccion(seccion);
+				break;
+
+			case 0:
+				System.out.println("Ha salido");
+				break;
+			}
+		} while (opcion != 0);
+
+	}
+}
---
+++ Video_Tutorial/src/video/Tienda.java
@@ -0,0 +1,167 @@
+package video;
+
+import java.util.Arrays;
+
+public class Tienda {
+
+	private Producto[] listado;
+	private int numProductos;
+	private int numTrabajadores;
+
+	public Tienda(Producto[] listado, int numProductos, int numTrabajadores) {
+		this.listado = listado;
+		this.numProductos = numProductos;
+		this.numTrabajadores = numTrabajadores;
+	}
+
+	public Producto[] getListado() {
+		return listado;
+	}
+
+	public void setListado(Producto[] listado) {
+		this.listado = listado;
+	}
+
+	public int getNumProductos() {
+		return numProductos;
+	}
+
+	public void setNumProductos(int numProductos) {
+		this.numProductos = numProductos;
+	}
+
+	public int getNumTrabajadores() {
+		return numTrabajadores;
+	}
+
+	public void setNumTrabajadores(int numTrabajadores) {
+		this.numTrabajadores = numTrabajadores;
+	}
+
+	@Override
+	public String toString() {
+		return "Tienda [listado=" + Arrays.toString(listado) + ", numProductos=" + numProductos + ", numTrabajadores="
+				+ numTrabajadores + "]";
+	}
+	
+	/**
+	 * Este metodo sirve para añadir un producto creado por el usario en la lista
+	 * @param producto El producto creado por el usario
+	 */
+	public void add(Producto producto) {
+
+		listado[numProductos++] = producto;
+	}
+
+	/**
+	 * Este metodo sirve para mostrar el array que indiquemos
+	 * @param lista El array que queremos mostrar
+	 */
+	public void mostrarArray(Producto[] lista) {
+		for (int i = 0; i < lista.length; i++) {
+			if (lista[i] != null) {
+				System.out.println(lista[i]);
+			}
+		}
+	}
+	/**
+	 * Este metodo solo muestra los productos de un array
+	 * que esten en venta
+	 */
+	public void mostrarActivos() {
+		for (int i = 0; i < numProductos; i++) {
+			if (listado[i].isEnVenta()) {
+				System.out.println(listado[i]);
+			}
+		}
+	}
+	/**
+	 * Metodo que busca un producto por su id y devuelve el índice del producto
+	 * @param id del producto a buscar
+	 * @return
+	 */
+	public int buscarID(int id) {
+		int i = 0;
+		boolean encontrado = false;
+		while (i < numProductos && !encontrado) {
+			if (listado[i].getId() == (id))
+				encontrado = true;
+			else
+				i++;
+		}
+		if (encontrado)
+			return i;
+		else
+			return -1;
+	}
+
+	
+	public Producto buscarIDV2(int id) {
+		int i = 0;
+		boolean encontrado = false;
+		while (i < numProductos && !encontrado) {
+			if (listado[i].getId() == (id))
+				encontrado = true;
+			else
+				i++;
+		}
+		if (encontrado)
+			return listado[i];
+		else
+			return null;
+	}
+
+	
+	public Producto[] buscarBySeccion(int seccion) {
+		int i = 0;
+
+		Producto[] listaEncontrados = new Producto[numProductos];
+		while (i < numProductos) {
+
+			if (listado[i].getSeccion() == seccion) {
+				listaEncontrados[i] = listado[i];
+			}
+			i++;
+		}
+		return listaEncontrados;
+	}
+
+	public double calcularPrecioFinal(double ganancia, int id) {
+		Producto producto;
+		producto = buscarIDV2(id);
+		double cien = 100;
+
+		return producto.getPrecioFabrica() + producto.getPrecioFabrica() * ganancia / cien;
+	}
+
+	
+	public void modificarPrecioPorSeccion(int seccion, double nuevoPrecio) {
+
+		for (int i = 0; i < numProductos; i++) {
+			if (listado[i].getSeccion() == seccion) {
+				listado[i].setPrecioFabrica(nuevoPrecio);
+			}
+		}
+	}
+
+	public double hacerDescuento(double descuento, double ganancias, int id) {
+
+		double cien = 100;
+		double resultado;
+
+		resultado = calcularPrecioFinal(ganancias, id) - calcularPrecioFinal(ganancias, id) * descuento / cien;
+
+		return resultado;
+	}
+
+	public int eliminarUnaSeccion(int seccion) {
+		int eliminados = 0;
+		for (int i = 0; i < numProductos; i++) {
+			if (listado[i].getSeccion() == seccion) {
+				listado[i].setEnVenta(false);
+				eliminados++;
+			}
+		}
+		return eliminados;
+	}
+}
